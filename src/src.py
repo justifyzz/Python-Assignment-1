18d357ad48d89fdcc12c3079af58ae845e8b77ae
justifyzz <80364793+justifyzz@users.noreply.github.com>
2021-09-26 23:01:50 +0600

    Update src.py

--- src/src.py
+++ src/src.py
@@ -1,1 +1,15 @@
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+res = cg.get_coins_markets(vs_currency='usd')
+def myFunction(length):
+    listOfMarketCaps = []
+    listOfNames = []
+    for i in range(length):
+        listOfMarketCaps.append(int(res[i]['market_cap']))
+        listOfNames.append(res[i]['name'])
 
+    for i in range(length):
+        print(i + 1, ':', listOfNames[i], listOfMarketCaps[i])
+
+number = int(input('Enter the number of coins: '))
+myFunction(number)
