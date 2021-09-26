8386203a9cf7099754586c26ba6646ec77dc6165
justifyzz <80364793+justifyzz@users.noreply.github.com>
2021-09-26 23:25:31 +0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,7 @@
+from pycoingecko import CoinGeckoAPI
+
+number = int(input('Enter the number of coins: '))
+
+  for i in range(length):
+        print(i + 1, ':', listOfNames[i], listOfMarketCaps[i])
 
